b091b2876ee3e83335fba118200b889e30b5b332
Erwin GO <erwingaitano@gmail.com>
2017-06-26 22:59:25 -0500

    adding the program functionality

---
+++ src/index.js
@@ -0,0 +1,71 @@
+#! /usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const commander = require('commander');
+const glob = require('glob');
+// TODO: you should only use helpers in your same level or above
+const helpers = require('./FolderChecker/_helpers');
+const filenameChecker = require('./FolderChecker/filename');
+const componentChecker = require('./FolderChecker/component');
+const moduleChecker = require('./FolderChecker/module');
+const singleFolderChecker = require('./FolderChecker/singleFolder');
+const version = require('../package.json').version;
+
+commander.version(version)
+  .usage('[options] <path>')
+  .option(
+    '-if, --ignore-folders <folders>',
+    'folders to ommit, should be a string, it accepts glob patterns'
+  )
+  .parse(process.argv);
+
+if (commander.args.length !== 1) {
+  commander.help();
+} else {
+  try {
+    const rootPath = path.resolve(commander.args[0]);
+
+    if (!fs.statSync(rootPath).isDirectory()) throw new Error(`${rootPath}, should be a directory`);
+
+    let { files, folders } = helpers.getAllFilesAndFolders(rootPath, true);
+
+    if (commander.ignoreFolders) {
+      const ignoreFolders = glob.sync(commander.ignoreFolders).map(el => path.resolve(el));
+      files = files.filter(el => !ignoreFolders.some(el2 => el.indexOf(el2) === 0));
+      folders = folders.filter(el => !ignoreFolders.some(el2 => el.indexOf(el2) === 0));
+    }
+
+    // folders/filenames validations
+
+    files.forEach(filenameChecker.validateFilename);
+    folders.forEach(el => filenameChecker.validateFilename(el, true));
+
+    // Module validation
+
+    moduleChecker.validateModule(rootPath);
+
+    // single folder validations
+
+    folders.filter(el => el.split('/').pop() === '_css')
+      .forEach(singleFolderChecker.validateUnderscoreCssFolder);
+
+    folders.filter(el => el.split('/').pop() === '_helpers')
+      .forEach(singleFolderChecker.validateUnderscoreHelpersFolder);
+
+    folders.filter(el => el.split('/').pop() === '_fonts')
+      .forEach(singleFolderChecker.validateUnderscoreFontsFolder);
+
+    folders.filter(el => el.split('/').pop() === '_media')
+      .forEach(singleFolderChecker.validateUnderscoreMediaFolder);
+
+    folders.filter(el => el.split('/').pop() === '_vendors')
+      .forEach(singleFolderChecker.validateUnderscoreVendorsFolder);
+
+    folders.filter(el => el.split('/').pop() === '_components')
+      .forEach(componentChecker.validateUnderscoreComponentsFolder);
+  } catch (err) {
+    console.error('\n', err.message, '\n\n');
+    throw err;
+  }
+}
