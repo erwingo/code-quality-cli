ba0a2c7ca691e912378cf5e96d45a0f1711cc0ca
Erwin GO <erwingaitano@gmail.com>
2017-06-30 20:50:51 -0500

    adding tests to the requireChecker module

--- src/requireChecker/_tests/index.js
+++ src/requireChecker/_tests/index.js
@@ -1,3 +1,30 @@
+const path = require('path');
+const assert = require('assert');
+const requireChecker = require('../index');
+
 describe('requireChecker', () => {
-  it('should throw because a js file is not being required anywhere');
+  it('should pass all validations', () => {
+    assert.doesNotThrow(
+      () => {
+        const ignoreFiles = [path.join(__dirname, 'examples/projectGood1/index.js')];
+        requireChecker.run(path.join(__dirname, 'examples/projectGood1'), [], ignoreFiles);
+      }
+    );
+  });
+
+  it('should throw because a js file is not being required anywhere', () => {
+    assert.throws(
+      () =>
+        requireChecker.run(path.join(__dirname, 'examples/projectBad1')),
+      err => err.message.includes('projectBad1/index.js, is not being required')
+    );
+
+    const ignoreFiles = [path.resolve(__dirname, 'examples/projectBad2/index.js')];
+
+    assert.throws(
+      () =>
+        requireChecker.run(path.join(__dirname, 'examples/projectBad2'), [], ignoreFiles),
+      err => err.message.includes('projectBad2/src/landingpages/awesomething.js, is not being required')
+    );
+  });
 });
